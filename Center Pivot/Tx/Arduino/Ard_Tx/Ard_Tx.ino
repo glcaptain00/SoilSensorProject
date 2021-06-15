ad05083fbda9037f592cab4c81536dea71f4de34
glcaptain00 <glcaptain00@gmail.com>
2021-06-15 14:34:50 -0500

    Restructured packets to leave space for RSSI from sensor to relay

--- Center Pivot/Tx/Arduino/Ard_Tx/Ard_Tx.ino
+++ Center Pivot/Tx/Arduino/Ard_Tx/Ard_Tx.ino
@@ -1,6 +1,6 @@
 //Configurables
 #define TX_POWER 13
-#define PIN_SENSOR 1
+#define PIN_SENSOR A1
 
 //CONSTANTS
 #define PACKET_LENGTH 18
@@ -9,8 +9,7 @@
 #include <SPI.h>
 
 //UUID, don't touch. UUID: 4918bc6e-c881-11eb-b8bc-0242ac130003
-uint8_t uuid [16] = {0x49, 0x18, 0xbc, 0x6e, 0xc8, 0x81, 0x11, 0xeb, 0xb8, 0xbc, 0x02, 0x42, 0xac, 0x13, 0x00, 0x03};
-
+uint8_t id = 0x00;
 RH_RF95 rf95; //Creating global object for the radio
 void setup() {
 	Serial.begin(9600); //Initialize serial port.
@@ -26,17 +25,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 	delay(500); //Delay in milliseconds.
-	int voltage = 0x0ABC; //FIXME: This needs to read from the sensor, not be hard coded.
-	uint8_t data [PACKET_LENGTH] = {}; //Create an array of PACKET_LENGTH size. See 'constants' macro section (at top)
-	uint8_t of = 0x00; //Store overflow variable for right shift compression
-	uint8_t next_of = 0x00; //temp overflow. Necessary for cycling without creating a new array.
-	uint16_t temp = ((voltage & 0x3FF) << 1) | 0x0000; //Left shifting one gives the overflow of right shifting 7
-	data[PACKET_LENGTH - 1] = temp & 0xFF; //Src bit is 0, so 
-	data[PACKET_LENGTH - 2] = temp >> 8;
-	for (int i = PACKET_LENGTH - 3; i >= 0; i--)
-	{
-		data[i+1] = data[i+1] | (uuid[i] << 3);
-		data[i] = uuid[i] >> 5;
-	}
-	rf95.send(data, sizeof(data)); //Send the data.
+	int voltage = analogRead(PIN_SENSOR); //FIXME: This needs to read from the sensor, not be hard coded.
+	uint8_t* data = new uint8_t[4];
+ 
+	data[3] = id; //ID byte
+  data[2] = 0x00; //RSSI byte
+  data[1] = voltage & 0xFF;
+  data[0] = voltage >> 8;
+	rf95.send(data, 4); //Send the data.
 }
