2586d231a707dcd54843f7ca27a9dfcae075b6ff
glcaptain00 <glcaptain00@gmail.com>
2021-07-22 14:03:49 -0500

    Adjusted to new file name convention
    Opens files from stage 3. Outputs stage 4

--- Drone/Data/DroneCoordToFeet/Convert.py
+++ Drone/Data/DroneCoordToFeet/Convert.py
@@ -1,23 +1,28 @@
 import math
+import os
 
 #CONSTANTS
 earthRadius = 3958.8 #miles
 
-input = open("input.csv", "r")
-output = open("output.csv", "w")
+dir = os.listdir()
+for file in dir:
+    if not (file.startswith("3-")):
+        continue
+    input = open(file, "r")
+    output = open(file.replace("3-","4-"), "w")
 
-output.write(input.readline())
+    output.write(input.readline())
 
-def coordOffset(latOrLong): #Takes in either that latitude or longitude of a point and gives an approximate distance to that point in the same direction in feet.
-    rad = latOrLong * (math.pi / 180)
-    return rad * 3958.8 * 5280
-    
-    
-print(coordOffset(0.00005))
+    def coordOffset(latOrLong): #Takes in either that latitude or longitude of a point and gives an approximate distance to that point in the same direction in feet.
+        rad = latOrLong * (math.pi / 180)
+        return rad * 3958.8 * 5280
+        
+        
+    print(coordOffset(0.00005))
 
 
-for line in input:
-    dat = line.split(",")
-    latInFeet = coordOffset(float(dat[2]))
-    longInFeet = coordOffset(float(dat[3]))
-    output.write("{},{},{},{},{}".format(dat[0],dat[1],latInFeet,longInFeet,dat[4]))
+    for line in input:
+        dat = line.split(",")
+        latInFeet = coordOffset(float(dat[2]))
+        longInFeet = coordOffset(float(dat[3]))
+        output.write("{},{},{},{},{}".format(dat[0],dat[1],latInFeet,longInFeet,dat[4]))
